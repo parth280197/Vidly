d173c34a9eefb2b8ce3b8ed0311a33895bac07e3
parth <parth280197@gmail.com>
2019-08-11 19:50:12 -0500

    AutoMapperAdded

--- Vidly/App_Start/WebApiConfig.cs
+++ Vidly/App_Start/WebApiConfig.cs
@@ -1,21 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System.Web.Http;
 
 namespace Vidly
 {
-    public static class WebApiConfig
+  public static class WebApiConfig
+  {
+    public static void Register(HttpConfiguration config)
     {
-        public static void Register(HttpConfiguration config)
-        {
-            config.MapHttpAttributeRoutes();
+      var settings = config.Formatters.JsonFormatter.SerializerSettings;
+      settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+      settings.Formatting = Formatting.Indented;
+      config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
+      config.Routes.MapHttpRoute(
+          name: "DefaultApi",
+          routeTemplate: "api/{controller}/{id}",
+          defaults: new { id = RouteParameter.Optional }
+      );
     }
+  }
 }
--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -1,8 +1,9 @@
+using AutoMapper;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using System.Data.Entity;
+using Vidly.App_Start;
 
 namespace Vidly
 {
@@ -10,6 +11,7 @@ namespace Vidly
   {
     protected void Application_Start()
     {
+      Mapper.Initialize(c => c.AddProfile<MappingProfile>());
       GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings
         .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
       GlobalConfiguration.Configuration.Formatters
