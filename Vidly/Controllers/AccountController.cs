b6ff44f33c7077195ad5e89877b36f0f30cb0dfe
parth <parth280197@gmail.com>
2019-08-23 22:21:49 -0500

    Phone number added in reagistration form

--- Vidly/Controllers/AccountController.cs
+++ Vidly/Controllers/AccountController.cs
@@ -148,7 +148,12 @@ namespace Vidly.Controllers
     {
       if (ModelState.IsValid)
       {
-        var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+        var user = new ApplicationUser
+        {
+          UserName = model.Email,
+          Email = model.Email,
+          PhoneNumber = model.PhoneNumber
+        };
         var result = await UserManager.CreateAsync(user, model.Password);
         if (result.Succeeded)
         {
